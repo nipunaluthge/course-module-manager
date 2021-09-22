0416034bc8fa2bfcb0299304e5c845a528ada5e4
nipunaluthge <nipunaluthge4815@gmail.com>
2021-09-22 22:51:56 +0530

    working on login

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import HomePageContainer from './Constants/HomePageContainer';
+import HomePageContainer from './Containers/HomePageContainer';
+import LoginFormComponent from './Components/LoginFormComponent';
 
 function App() {
   return (
     <div className="App">
       <HomePageContainer />
+      <LoginFormComponent />
     </div>
   );
 }
--- src/Components/LoginFormComponent.jsx
+++ src/Components/LoginFormComponent.jsx
@@ -3,11 +3,15 @@ import React from 'react';
 const LoginFormComponent = () => {
   return (
     <div>
+      <div className="login-heading">Login</div>
       <div className="password-login-section">
         <form action="" className="loging-form">
           <input type="email" className="login-email" placeholder="Email address" />
           <input type="text" className="login-password" placeholder="Password" />
         </form>
+      </div>
+      <div className="google-login-section">
+        <button>Login with google</button>
       </div>
     </div>
   )
