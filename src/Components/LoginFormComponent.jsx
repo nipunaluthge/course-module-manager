b9f0b783cde369a642d2b4dcc9a20a4e35905722
nipunaluthge <nipunaluthge4815@gmail.com>
2021-09-22 22:19:53 +0530

    initial commit

---
+++ src/Components/LoginFormComponent.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const LoginFormComponent = () => {
+  return (
+    <div>
+      <div className="password-login-section">
+        <form action="" className="loging-form">
+          <input type="email" className="login-email" placeholder="Email address" />
+          <input type="text" className="login-password" placeholder="Password" />
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default LoginFormComponent;
---
+++ src/Constants/HomePageContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePageContainer = () => {
+  return (
+    <div>
+      Welcome to module manager
+    </div>
+  )
+}
+
+export default HomePageContainer;
