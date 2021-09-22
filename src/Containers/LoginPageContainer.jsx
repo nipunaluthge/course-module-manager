cb9c5a90f0bdd1af67cdaa5f15ce8198561c18d8
nipunaluthge <nipunaluthge4815@gmail.com>
2021-09-22 22:43:49 +0530

    working on login

---
+++ src/Containers/LoginPageContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginFormComponent from '../Components/LoginFormComponent';
+
+const LoginPageContainer = () => {
+  return (
+    <div>
+      <LoginFormComponent />
+    </div>
+  )
+}
+
+export default LoginPageContainer;
